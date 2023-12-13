0128c073d01ce9aa051fa3d4efdde431f18e25cd
David Dunne <x00167239@mytudublin.ie>
2023-12-13 23:44:08 +0000

    Updated scroll bar

--- MatchScore/obj/Debug/net7.0/MatchScore.AssemblyInfo.cs
+++ MatchScore/obj/Debug/net7.0/MatchScore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277df2c24c79d7f209fe90fa60edda7f881467e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
