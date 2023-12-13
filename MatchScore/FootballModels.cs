dd592dad3091d816ebed1ddb96b0a09b30fa9d0f
DavidDunne165 <91316635+DavidDunne165@users.noreply.github.com>
2023-12-13 19:59:39 +0000

    Update FootballModels.cs

--- MatchScore/FootballModels.cs
+++ MatchScore/FootballModels.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using static System.Formats.Asn1.AsnWriter;
-
+// Models
 namespace MatchScore.Models
 {
     public class FixtureResponse
